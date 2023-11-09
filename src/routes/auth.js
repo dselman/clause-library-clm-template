31d7bddeb0fa3220f4ccc1e816912535aa8062bd
Dan Selman <danscode@selman.org>
2023-11-09 15:42:05 +0000

    routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 export { router as AuthRouter };
 
-router.get("/auth/callback", (req, res) => {
+router.get("/callback", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ a: 1 }, null, 3));
 });
